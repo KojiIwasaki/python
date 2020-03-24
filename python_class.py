f41824803aaec2856e397ba974172d1249829bf1
Kojiiwasaki <cest-lavie@outlook.jp>
2020-03-24 12:47:25 +0900

    クラスの練習(1)追加

---
+++ python_class.py
@@ -0,0 +1,10 @@
+class myclass():
+    def mynameaddress(self, who, jyusho):
+        self.name = who
+        self.address = jyusho
+
+person1 = myclass()
+
+person1.mynameaddress("Yamada","Tokyo")
+
+print(type(myclass))
