b9099ffd6962c60af6c89eb84b793769edd118d2
Kojiiwasaki <cest-lavie@outlook.jp>
2020-04-01 12:41:32 +0900

    python練習完結

--- python_path.py
+++ python_path.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 
+terms = {'for':0, 'if':0, 'else':0}
 path = Path()
 for filepath in path.glob('*.py'):
-    print(filepath)
+    with open(filepath, encoding='utf-8') as rfile:
+        text = rfile.read()
+    for term in terms:
+        cnt = text.count(term)
+        terms[term] += cnt
+
+print(terms)
